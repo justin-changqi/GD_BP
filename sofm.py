53f21c6572397f2ae9f03049f9bc6e23f5a1206c
Justin, Chang-Qi Zhang <justin840727@gmail.com>
2018-05-07 02:07:15 +0800

    add code for problem1

---
+++ sofm.py
@@ -0,0 +1,16 @@
+import math
+
+if __name__ == "__main__":
+    eta0 = 0.1
+    sd0 = 0.1
+    _lambda = 10
+    t=0.
+    result = 0
+    while True:
+        sd = sd0 * math.exp(t/10.)
+        result = eta0*math.exp(-t/_lambda)*math.exp(1./pow(sd,2)*2)
+        t += 1
+        if (result < 0.001):
+            break
+    print ('min iterative: ', t)
+    print ('learning rate: ', result)
