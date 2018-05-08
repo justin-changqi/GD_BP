be47d291327d7623644d53f52e1a5e791675b941
Justin, Chang-Qi Zhang <justin840727@gmail.com>
2018-05-08 12:00:22 +0800

    fix functions

--- sofm.py
+++ sofm.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     result = 0
     while True:
         sd = sd0 * math.exp(t/10.)
-        result = eta0*math.exp(-t/_lambda)*math.exp(1./pow(sd,2)*2)
+        result = eta0*math.exp(-t/_lambda)*math.exp(1./(pow(sd,2)*2))
         t += 1
         if (result < 0.001):
             break
